757aeb4f3b92131983224897d15cd95c0e49f0d3
Andrew Lobanov <spline1986@yandex.ru>
2021-04-11 15:41:28 +0500

    Localization switched to Russian

--- gallery/settings.py
+++ gallery/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for gallery project.
 
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'Asia/Yekaterinburg'
 
